951bd5ddec8f153a8dcce05788e245adc3902db8
Carlo Schmidt <carlo.schmidt@capgemini.com>
2024-11-20 09:56:26 +0100

    makefile in Basecommand eingebunden

--- src/main/java/cmd/commands/BaseCommand.java
+++ src/main/java/cmd/commands/BaseCommand.java
@@ -5,6 +5,7 @@ import cmd.commands.copy.CopyCommand;
 import cmd.commands.del.DelCommand;
 import cmd.commands.dir.DirCommand;
 import cmd.commands.mkd.MakeDirCommand;
+import cmd.commands.mkf.MakeFile;
 import picocli.CommandLine.Command;
 
 /**
@@ -20,7 +21,7 @@ import picocli.CommandLine.Command;
         name = "cmd",
         description = "base command",
         mixinStandardHelpOptions = true,
-        subcommands = {DirCommand.class, DelCommand.class, CopyCommand.class, cdCommand.class, MakeDirCommand.class})
+        subcommands = {DirCommand.class, DelCommand.class, CopyCommand.class, cdCommand.class, MakeDirCommand.class, MakeFile.class})
 public class BaseCommand implements Runnable {
     public BaseCommand() {
         /* intentionally empty */
--- src/main/java/cmd/commands/mkf/MakeFile.java
+++ src/main/java/cmd/commands/mkf/MakeFile.java
@@ -2,17 +2,22 @@ package cmd.commands.mkf;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import picocli.CommandLine;
 import picocli.CommandLine.Parameters;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 
+@CommandLine.Command(
+        name = "mkf",
+        description = "Create a file.",
+        mixinStandardHelpOptions = true)
 public class MakeFile implements Runnable {
 
     private static final Logger LOG = LoggerFactory.getLogger(MakeFile.class);
 
-    @Parameters(index = "0", description = "path of the file to copy")
+    @Parameters(index = "0", description = "path of the file to create")
     private File file;
 
 
