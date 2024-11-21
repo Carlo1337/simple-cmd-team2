8cdedb129d09a92930df7b855233e38b07618701
bsiebenk <benjamin.siebenkees@sogeti.com>
2024-11-21 09:52:27 +0100

    cd Command

--- src/main/java/cmd/commands/BaseCommand.java
+++ src/main/java/cmd/commands/BaseCommand.java
@@ -5,6 +5,7 @@ import cmd.commands.copy.CopyCommand;
 import cmd.commands.del.DelCommand;
 import cmd.commands.dir.DirCommand;
 import cmd.commands.mkd.MakeDirCommand;
+import cmd.commands.move.moveCommand;
 import picocli.CommandLine.Command;
 
 /**
@@ -20,7 +21,7 @@ import picocli.CommandLine.Command;
         name = "cmd",
         description = "base command",
         mixinStandardHelpOptions = true,
-        subcommands = {DirCommand.class, DelCommand.class, CopyCommand.class, cdCommand.class, MakeDirCommand.class})
+        subcommands = {DirCommand.class, DelCommand.class, CopyCommand.class, cdCommand.class, MakeDirCommand.class, moveCommand.class})
 public class BaseCommand implements Runnable {
     public BaseCommand() {
         /* intentionally empty */
