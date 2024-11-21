0c3b9749e22923ac2c86d5f2355d1449e56f8800
quangple <quang-phu-quang.le@capgemini.coom>
2024-11-21 09:47:07 +0100

    copy command warning if overwrite existing file

--- src/main/java/cmd/commands/copy/CopyCommand.java
+++ src/main/java/cmd/commands/copy/CopyCommand.java
@@ -1,12 +1,15 @@
 package cmd.commands.copy;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Parameters;
 
 import java.io.File;
-import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
+import java.util.Scanner;
 
 /**
  * "Copy File" command class
@@ -20,6 +23,8 @@ import java.nio.file.StandardCopyOption;
         mixinStandardHelpOptions = true)
 public class CopyCommand implements Runnable {
 
+    public static final Logger LOG = LoggerFactory.getLogger(CopyCommand.class);
+
     @Parameters(index = "0", description = "path of the file to copy")
     private File source;
 
@@ -32,10 +37,29 @@ public class CopyCommand implements Runnable {
 
     @Override
     public void run() {
+        if(target.exists()) {
+            LOG.info("{} already exists. Overwrite? (yes/no): ", target.getAbsolutePath());
+            try (Scanner scanner = new Scanner(System.in)) {
+                String response = scanner.nextLine().trim().toLowerCase();
+                if (response.equals("yes")) {
+                    copy(source.toPath(), target.toPath());
+                } else if (response.equals("no")) {
+                    LOG.info("Copy canceled.\n");
+                }
+            } catch (Exception e) {
+                LOG.error("Error", e);
+            }
+        } else {
+            copy(source.toPath(), target.toPath());
+        }
+    }
+
+    private void copy(Path source, Path target) {
         try {
-            Files.copy(source.toPath(), target.toPath(), StandardCopyOption.REPLACE_EXISTING);
-        } catch (IOException e) {
-            e.printStackTrace();
+            Files.copy(source, target, StandardCopyOption.REPLACE_EXISTING);
+            LOG.info("{} copied successfully.\n", target.toAbsolutePath());
+        } catch (Exception e) {
+            LOG.info("Error copying the file {}: \n", source.toAbsolutePath());
         }
     }
 }
